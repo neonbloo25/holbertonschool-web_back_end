ee79668055f60bd81817b857a925c6c264fa2f69
neonbloo25 <theluckdemon@gmail.com>
2025-03-29 23:16:17 -0400

    re 0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,15 +1,14 @@
 export function taskFirst() {
     const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
+    return task; 
+}
+
+export function getLast() {
+    return ' is okay'; 
+}
+
+export function taskNext() {
     let combination = 'But sometimes let';
-    combination += getLast();
+    combination += getLast(); 
     return combination;
-  }
-  
+}
