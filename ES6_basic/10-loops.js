96034dea8b218f20daf3a1a85c0df503b89c2c5f
neonbloo25 <theluckdemon@gmail.com>
2025-03-30 13:33:35 -0400

    to 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
 
-  for (var idx in array) {
+  for (const value of array) {
     newArray.push(appendString + value);
   }
 
