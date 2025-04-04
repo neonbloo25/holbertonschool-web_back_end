f627e7b4871b9a0e8276dd1509f9f9781d5d6a51
neonbloo25 <theluckdemon@gmail.com>
2025-04-04 09:06:02 -0400

    re 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,12 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const results = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ]);
-  return results.map(result => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+  return Promise.allSettled([promise1, promise2]).then((results) => results.map((result) => {
+    const newObject = {
+      status: result.status,
+    };
+    if (result.status === 'fulfilled') {
+      newObject.value = result.value;
+    } else {
+      newObject.value = result.reason.toString();
+    }
+    return newObject;
   }));
 }
