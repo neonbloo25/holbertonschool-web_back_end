ecf0ee3a760a9506303f83b3c2eb37745fab4110
neonbloo25 <theluckdemon@gmail.com>
2025-04-03 21:32:18 -0400

    to 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+// 4-update_grade_by_city.js
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+        const grade = newGrades.find((g) => g.studentId === student.id);
+        student.grade = grade ? grade.grade : 'N/A';
+        return student;
+    });
+}
+
+export default updateStudentGradeByCity;
