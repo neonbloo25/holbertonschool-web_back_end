0ffa2a2f355bc1e52f931ff4ee8ceacc266c9c03
neonbloo25 <theluckdemon@gmail.com>
2025-04-04 09:03:03 -0400

    re 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    console.log(photo.body, user.firstName, user.lastName);
-  } catch (error) {
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    const [photo, user] = values;
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  }).catch (() => {
     console.log('Signup system offline');
-  }
+  });
 }
