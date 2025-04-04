a610c0e2cce88944e95ba3a7539bbdcbfd57ffb8
neonbloo25 <theluckdemon@gmail.com>
2025-04-03 23:06:33 -0400

    re 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 // 8-clean_set.js
 function cleanSet(set, startString) {
-    const result = [...set]
-      .filter(item => item.startsWith(startString))
-      .map(item => item.slice(startString.length))
-      .join('-');
-    return result;
-  }
-  
-  export default cleanSet;
+  const result = [...set]
+    .filter(item => item.startsWith(startString))
+    .map(item => item.slice(startString.length))
+    .join('-');
+  return result;
+}
+
+export default cleanSet;
   
