f8b4de7019091d4def8a44552ffaab317411e58a
neonbloo25 <theluckdemon@gmail.com>
2025-04-04 09:04:22 -0400

    re 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((values) => {
     const [photo, user] = values;
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  }).catch (() => {
+  }).catch(() => {
     console.log('Signup system offline');
   });
 }
