650ad36d9178b0423478dfefb74f467ba10720bd
neonbloo25 <theluckdemon@gmail.com>
2025-04-04 09:08:51 -0400

    re 9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,10 +1,28 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
-  try {
-    queue.push(mathFunction());
-  } catch (error) {
-    queue.push(error.message);
+    const queue = [];
+    try {
+      const result = mathFunction();  // Call the passed function
+      
+      if (result instanceof Promise) {
+        // If the result is a Promise, handle it with then() / catch()
+        result
+          .then((value) => {
+            queue.push(value);
+            queue.push('Guardrail was processed');
+          })
+          .catch((error) => {
+            queue.push(error.message);
+            queue.push('Guardrail was processed');
+          });
+      } else {
+        // If it's not a Promise, push the result directly
+        queue.push(result);
+        queue.push('Guardrail was processed');
+      }
+    } catch (error) {
+      queue.push(error.message);
+      queue.push('Guardrail was processed');
+    }
+    return queue;
   }
-  queue.push('Guardrail was processed');
-  return queue;
-}
+  
