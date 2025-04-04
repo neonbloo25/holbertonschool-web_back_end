3e1285ba6db1a0ae5cf82066a995be7ad0b363e8
neonbloo25 <theluckdemon@gmail.com>
2025-04-03 21:10:58 -0400

    to 2

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 // 2-get_students_by_loc.js
 function getStudentsByLocation(students, city) {
-  return students.filter((student => student.location === city);
+  return students.filter((student) => student.location === city);
 }
 
 export default getStudentsByLocation;
