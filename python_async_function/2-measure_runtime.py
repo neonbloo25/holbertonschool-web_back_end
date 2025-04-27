317d8ba087ba066917ed9216da2c0b64c5f6f118
neonbloo25 <theluckdemon@gmail.com>
2025-04-26 20:58:55 -0400

    re 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,6 +3,7 @@ import time
 import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """Return average execution time per coroutine."""
     start = time.time()
