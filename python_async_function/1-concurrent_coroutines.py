2955c4af525d5f03c4b600827a92ee76d1a891e6
neonbloo25 <theluckdemon@gmail.com>
2025-04-27 22:31:57 -0400

    re 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
+import time
 import asyncio
-from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Run wait_random n times concurrently and return delays"""
-    tasks = [asyncio.create_task(wR(max_delay)) for _ in range(n)]
-    delays = []
-
-    for task in asyncio.as_completed(tasks):
-        result = await task
-        delays.append(result)
-
-    return delays
+def measure_time(n: int, max_delay: int) -> float:
+    """Return average execution time per coroutine."""
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    return (end - start) / n
