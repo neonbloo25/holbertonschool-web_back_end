a0810b48eb013d8de1fd167ff3cb6b17f93efd82
neonbloo25 <theluckdemon@gmail.com>
2025-04-28 12:45:50 -0400

    re4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-"""Execute multiple asyncio tasks concurrently"""
+"""Asyncio time"""
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> list[float]:
-    """Run task_wait_random n times concurrently and return sorted list of results."""
+    """Target Function"""
+    delays: List[float] = []
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        delays.append(result)
+
+    return delays
