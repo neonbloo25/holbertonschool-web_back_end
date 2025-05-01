6f1eee79d3b2e3200ba2d0a7728060854ac719a8
neonbloo25 <theluckdemon@gmail.com>
2025-05-01 01:00:45 -0400

    re 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,12 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+async def wait_and_generate() -> float:
+    """Additional Function"""
+    await asyncio.sleep(1)
+    return random.uniform(0, 10)
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """Target Function"""
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.random() * 10
+        yield await wait_and_generate()
