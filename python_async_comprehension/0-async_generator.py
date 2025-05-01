41e464f470eaa0cfbddf5cdc526ddcf2b65ac9f0
neonbloo25 <theluckdemon@gmail.com>
2025-04-30 23:14:18 -0400

    re 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,8 +5,9 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield a random float between 0 and 10, 10 times with 1 second delay each"""
+    """Yield a random float(0-10), 10 x 1 sec delay each"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
