6fefe3418baaeec2cc39d6e9889490b379648533
neonbloo25 <theluckdemon@gmail.com>
2025-05-01 16:21:11 -0400

    to 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,7 +2,8 @@
 """Async comprehension module"""
 
 from typing import List
-from 0-async_generator import async_generator
+async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     """Collect 10 numbers from async_generator using async comprehension"""
