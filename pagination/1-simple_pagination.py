6e8304e1533b7045902a5eae7550f30a7de3d60b
neonbloo25 <theluckdemon@gmail.com>
2025-05-02 11:00:45 -0400

    to 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -22,7 +22,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return paginated dataset."""
+        """Return paginated dataset"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
