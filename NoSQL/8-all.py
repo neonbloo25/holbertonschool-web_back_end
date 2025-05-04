587cf8b2e9cc6fdbfdba6025c53532c36ad14537
neonbloo25 <theluckdemon@gmail.com>
2025-05-04 17:25:28 -0400

    to 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" List all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """Return list of all documents in collection"""
+    return list(mongo_collection.find())
