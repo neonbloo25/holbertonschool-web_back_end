4ff6d8b04ee01847bb46d8fd1dcaa7ba789814f9
neonbloo25 <theluckdemon@gmail.com>
2025-05-25 22:52:09 -0400

    to 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, content) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = content.split('\n').filter(line => line.trim() !== '');
+      const students = lines.slice(1).map(line => line.split(','));
+      const total = students.length;
+      const fields = {};
+
+      students.forEach(([first, , , field]) => {
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(first);
+      });
+
+      let output = `Number of students: ${total}`;
+      for (const [field, names] of Object.entries(fields)) {
+        output += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
+      }
+
+      resolve(output);
+    });
+  });
+}
+
+const app = express();
+const path = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const data = await countStudents(path);
+    res.send(`This is the list of our students\n${data}`);
+  } catch (e) {
+    res.send('This is the list of our students\nCannot load the database');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
