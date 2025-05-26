fd2992fabe045c440d515646819186a06a2ac4e5
neonbloo25 <theluckdemon@gmail.com>
2025-05-25 23:37:58 -0400

    re 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write(`Your name is: ${name}`);
   // Wait a moment before printing the next line
   setTimeout(() => {
     console.log('\nThis important software is now closing');
