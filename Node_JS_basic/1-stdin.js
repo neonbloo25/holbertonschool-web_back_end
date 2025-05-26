0d4f89016a3504581fae0e832c61e7584bbbcc9f
neonbloo25 <theluckdemon@gmail.com>
2025-05-25 23:28:46 -0400

    re 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,11 @@
-process.stdin.setEncoding('utf8');
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('data', (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\r`);
+  // Wait a moment before printing the next line
+  setTimeout(() => {
+    console.log('\nThis important software is now closing');
+    process.exit();
+  }, 10); // Delay ensures first assertion is completed
 });
